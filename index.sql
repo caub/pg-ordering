c7a48696a5bcc7d485b4694fe420f7b25bb39609
caub <cyril.auburtin@gmail.com>
2018-01-13 22:08:49 +0100

    more

--- index.sql
+++ index.sql
@@ -43,12 +43,16 @@ AS $f$
   END;
 $f$;
  
--- insert or move item ACT_ID in category CAT_ID next to REL_ID,
+
+
+-- assumes the table have the INT columns p and q, with an exclusion contraint (see test)
+
+-- insert or move item ID next to REL_ID,
 -- before it if IS_BEFORE is true, otherwise after. REL_ID may
 -- be null to indicate a position off the end of the list.
  
-CREATE OR REPLACE FUNCTION cat_place_item(cat_id INTEGER,
-                                          act_id INTEGER,
+CREATE OR REPLACE FUNCTION place_item(_tbl regclass,
+                                          id INTEGER,
                                           rel_id INTEGER,
                                           is_before BOOLEAN)
   RETURNS void
@@ -63,13 +67,13 @@ AS $f$
   BEGIN
  
     -- moving a record to its own position is a no-op
-    IF rel_id=act_id THEN RETURN; END IF;
+    IF rel_id=id THEN RETURN; END IF;
  
     -- if we're positioning next to a specified row, it must exist
     IF rel_id IS NOT NULL THEN
-      SELECT cm.p, cm.q INTO strict p1, q1
-        FROM category_member cm
-       WHERE cm.category_id=cat_id AND cm.thing_id=rel_id;
+      SELECT t.p, t.q INTO strict p1, q1
+        FROM _tbl t
+       WHERE t.id=rel_id;
       r_rel := p1::float8 / q1;
     END IF;
  
@@ -77,15 +81,15 @@ AS $f$
     -- (might not exist).
     IF is_before THEN
       p2 := p1; q2 := q1;
-      SELECT cm2.p, cm2.q INTO p1, q1
-        FROM category_member cm2
-       WHERE cm2.category_id=cat_id AND cm2.thing_id <> act_id
+      SELECT t2.p, t2.q INTO p1, q1
+        FROM _tbl t2
+       WHERE t2.id <> id
          AND (p::float8/q) < COALESCE(r_rel,'infinity')
        ORDER BY (p::float8/q) DESC LIMIT 1;
     ELSE
-      SELECT cm2.p, cm2.q INTO p2, q2
-        FROM category_member cm2
-       WHERE cm2.category_id=cat_id AND cm2.thing_id <> act_id
+      SELECT t2.p, t2.q INTO p2, q2
+        FROM _tbl t2
+       WHERE t2.id <> id
          AND (p::float8/q) > COALESCE(r_rel,0)
        ORDER BY (p::float8/q) ASC LIMIT 1;
     END IF;
@@ -95,10 +99,10 @@ AS $f$
                                                COALESCE(p2,1),COALESCE(q2,0));
  
     -- move or insert the specified row
-    UPDATE category_member
-       SET (p,q) = (np,nq) WHERE category_id=cat_id AND thing_id=act_id;
+    UPDATE _tbl t
+       SET (p,q) = (np,nq) WHERE t.id=id;
     IF NOT found THEN
-      INSERT INTO category_member VALUES (cat_id, act_id, np, nq);
+      INSERT INTO t VALUES (id, np, nq);
     END IF;
  
     -- want to renormalize both to avoid possibility of integer overflow
@@ -107,51 +111,28 @@ AS $f$
     -- not requiring frequent normalization.
  
     IF (np > 10000000) OR (nq > 10000000) THEN
-      perform cat_renormalize(cat_id);
+      perform renormalize(_tbl, id);
     END IF;
   END;
 $f$;
  
--- Renormalize the fractions of items in CAT_ID, preserving the
--- existing order. The new fractions are not strictly optimal, but
--- doing better would require much more complex calculations.
---
--- the purpose of the complex update is as follows: we want to assign
+
+
+-- the purpose of the update is as follows: we want to assign
 -- a new series of values 1/2, 3/2, 5/2, ... to the existing rows,
--- maintaining the existing order, but because the unique expression
--- index is not deferrable, we want to avoid assigning any new value
--- that collides with an existing one.
---
--- We do this by calculating, for each existing row with an x/2 value,
--- which position in the new sequence it would appear at. This is done
--- by adjusting the value of p downwards according to the number of
--- earlier values in sequence. To see why, consider:
---
---   existing values:    3, 9,13,15,23
---   new simple values:  1, 3, 5, 7, 9,11,13,15,17,19,21
---                          *     *  *        *
---   adjusted values:    1, 5, 7,11,17,19,21,25,27,29,31
---
---   points of adjustment: 3, 7 (9-2), 9 (13-4, 15-6), 15 (23-8)
---
--- The * mark the places where an adjustment has to be applied.
---
--- Having calculated the adjustment points, the adjusted value is
--- simply the simple value adjusted upwards according to the number of
--- points passed (counting multiplicity).
+-- maintaining the existing order
  
-CREATE OR REPLACE FUNCTION cat_renormalize(cat_id INTEGER)
+CREATE OR REPLACE FUNCTION renormalize(_tbl regclass, id INTEGER)
   RETURNS void
   LANGUAGE plpgsql
   volatile strict
 AS $f$
   BEGIN
-    UPDATE category_member cm SET p = s2.new_rnum, q = 2 
-      FROM (SELECT thing_id, row_number() OVER (ORDER BY p::float8/q) AS new_rnum 
-              FROM category_member cm2 WHERE cm2.category_id=cat_id
+    UPDATE _tbl t SET p = s2.new_rnum, q = 2 
+      FROM (SELECT row_number() OVER (ORDER BY p::float8/q) AS new_rnum 
+              FROM _tbl t2 WHERE t2.id=id
           ) s2
-      WHERE s2.thing_id=cm.thing_id
-       AND s2.is_new
-       AND cm.category_id=cat_id;
+      WHERE s2.is_new
+       AND t.id=id;
   END;
 $f$;
--- test.sql
+++ test.sql
@@ -1,15 +1,21 @@
-CREATE TABLE categories (id INTEGER PRIMARY KEY, name text);
-CREATE TABLE things (id INTEGER PRIMARY KEY, name text);
+
  
-CREATE TABLE category_member (
-  category_id INTEGER NOT NULL REFERENCES categories,
-  thing_id INTEGER NOT NULL REFERENCES things ON DELETE cascade,
-  PRIMARY KEY (category_id,thing_id),
+CREATE TABLE todo_list (
+  id BIGSERIAL PRIMARY KEY,
+  text TEXT NOT NULL,
+
   p INTEGER NOT NULL, q INTEGER NOT NULL,
 
-  EXCLUDE USING btree ((p::float8/q) with =) DEFERRABLE;
+  EXCLUDE USING btree ((p::float8/q) WITH =) DEFERRABLE
 );
  
--- CREATE UNIQUE INDEX ON category_member (category_id, (p::float8/q));
+-- CREATE UNIQUE INDEX ON todo_list (id, (p::float8/q));
 
+INSERT INTO todo_list(text) VALUES ('foo'), ('bar'), ('qux'), ('wat');
+
+
+SELECT place_item(1, NULL, FALSE); -- insert foo at start
+SELECT place_item(2, 1, FALSE); -- insert bar after foo
+
+SELECT * FROM todo_list;
 
